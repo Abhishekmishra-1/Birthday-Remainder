b8c9ed4796783cac1bacbae4c59f2afb335dbe43
Abhishekmishra-1 <abhishekmishra1103@gmail.com>
2022-07-26 10:45:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Board from './components/Board';
+
+//https://www.youtube.com/watch?v=dYjdzpZv5yc&t=1782s
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
